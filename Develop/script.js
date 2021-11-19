7febb2c1809f56b8a9db3626da5bb3106e0c1397
Matt Reynolds <mattreynolds53@gmail.com>
2021-11-18 21:43:57 -0600

    Added final touches and cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -2,35 +2,13 @@ let currentDate = moment();
 $("#currentDay").text(currentDate.format("dddd, MMMM Do, YYYY"));
 
 let currentTime = moment().hour();
-// $("#currentTime").text(currentTime.format('h'));
 
 
-// Adding an event listener to save local storage
-// I can only seem to hook into the first button, so how can I hook into all buttons to avoid creating 15 different event listener functions? Also, I tried the "submit" functino, but it didn't work. I switched it to click and it started working (began logging to the console)
-
-
-//Statement to synch up time with color scheme of past, present, or future
-
-
-// let scheduleEntry = document.querySelectorAll("#scheduleEntry");
 let timeBlock = $("textarea")
 let saveBtn = $('button')
 
-//Saving textarea entry as a variable
-// let textAreaEntry = document.querySelector("#textAreaEntry").value;
-// console.log(textAreaEntry);
 
 
-// let saveTAEntry = document.querySelector("#saveBtn");
-
-// saveTAEntry.on("click", function(){
-//     console.log(`${textAreaEntry} saved to local storage`);
-//     localStorage.setItem("9AM", textAreaEntry);
-// }); 
-
-
-
-// Can we use >, <, and = with time? I'm not sure if it can tell that 1:00pm is greater than 12:00PM in its mind
 
 function setColor() {
     
@@ -68,6 +46,3 @@ function getTasks() {
 
 getTasks()
 setColor()
-
-// Give div for time section a data-vlue and compare that within the if statement
-
